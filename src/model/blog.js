265c32603a19ca572ab5de76a0bd39297a76e2f1
karankumarshreds <naileditnerd@gmail.com>
2020-12-06 23:04:57 +0530

    add blog model and route

--- src/model/blog.js
+++ src/model/blog.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const blogSchema = new mongoose.Schema({
   title: { type: String, default: null },
   description: { type: String, default: null },
+  user: { type: String, default: null },
 });
 
 module.exports = mongoose.model("Blog", blogSchema);
--- src/routes/blog.js
+++ src/routes/blog.js
@@ -1,11 +1,17 @@
 const express = require("express");
+const Blog = require("../model/blog");
 
 const router = express.Router();
 
 router.post("/api/blog", async (req, res) => {
-  const { title, description } = req.body;
-  console.log(title, description);
-  res.send({ ok: "ok" });
+  const { title, description, user } = req.body;
+  const blog = new Blog({
+    title,
+    description,
+    user,
+  });
+  await blog.save();
+  res.status(201).send(blog);
 });
 
 module.exports = router;
