eac67f52c6d6e70b001a4cfdfa023b3cba97c24c
karankumarshreds <naileditnerd@gmail.com>
2020-12-07 23:17:23 +0530

    transferring

--- src/routes/blog.js
+++ src/routes/blog.js
@@ -3,12 +3,14 @@ const util = require("util");
 const Blog = require("../model/blog");
 
 const router = express.Router();
+
 // redis config
 const redis = require("redis");
 const redisURL = "redis://127.0.0.1:6379";
 const client = redis.createClient(redisURL);
 // we are promisifying the get method because by default it uses callbacks
 // now we can use async and await
+client.get = util.promisify(client.get);
 
 router.post("/api/blog", async (req, res) => {
   const { title, description, user } = req.body;
@@ -28,22 +30,23 @@ router.get("/api/blog", async (req, res) => {
 
 router.get("/api/blog/user/:userId", async (req, res) => {
   // check if we have any cache data in redis for this query
-  let cachedBlogs;
-  client.get(req.params.userId, (err, val) => {
-    cachedBlogs = JSON.parse(val);
-    // if we have then fetch it from redis and return request
-    if (cachedBlogs) {
-      console.log("SERVING FROM CACHE", cachedBlogs);
-      return res.send(JSON.parse(cachedBlogs));
-    }
-  });
+  const cachedBlogs = await client.get(req.params.userId);
+  // if we have then fetch it from redis and return request
+  if (cachedBlogs) {
+    console.log("SERVED FROM CACHE", cachedBlogs);
+    return res.send(JSON.parse(cachedBlogs));
+  }
   // if not then reach out to mongodb and set cache in redis
   const blogs = await Blog.find({
     user: req.params.userId,
   });
-  console.log("SERVING FROM MONGODB");
+  console.log("SERVED FROM MONGO");
+  try {
+    client.set(req.params.userId, JSON.stringify(blogs));
+  } catch (error) {
+    console.log(error);
+  }
   res.send(blogs);
-  client.set(req.params.id, JSON.stringify(blogs));
 });
 
 module.exports = router;
