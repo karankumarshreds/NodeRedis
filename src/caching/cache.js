b2782958b820d00f825bbe9cf758d8957de9da2c
karankumarshreds <naileditnerd@gmail.com>
2020-12-09 00:07:44 +0530

    fix mongoose error

--- src/caching/cache.js
+++ src/caching/cache.js
@@ -1,10 +1,10 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // backup of default exec method
 const exec = mongoose.exec.prototype.exec;
 // overriding the default behaviour of exec
-mongoose.exec.prototype.exec = function () {
-  console.log('ADD CUSTOM LOGIC');
+mongoose.Query.prototype.exec = function () {
+  console.log("ADD CUSTOM LOGIC");
   // THEN run the default exec method
   return exec.apply(this, arguments);
-}
+};
--- src/server.js
+++ src/server.js
@@ -9,6 +9,17 @@ app.use(cors());
 app.use(express.json());
 app.use(router);
 
+/** Redis global config */
+
+// backup of default exec method
+const exec = mongoose.Query.prototype.exec;
+// overriding the default behaviour of exec
+mongoose.Query.prototype.exec = function () {
+  console.log("ADD CUSTOM LOGIC");
+  // THEN run the default exec method
+  return exec.apply(this, arguments);
+};
+
 const connectMongo = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
